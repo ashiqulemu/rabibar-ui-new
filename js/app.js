d6fab26a8a706dccacc720e26a81fe51b9fa0213
ashiqulemu <ashiqulemu.jpi@gmail.com>
2021-07-12 01:08:57 +0600

    spinner updated

--- js/app.js
+++ js/app.js
@@ -8,3 +8,14 @@
    console.log(event.target.classList.toggle('active'))
  
  }
+
+
+//  window.addEventListener('load', (event) => {
+//     let chart  = document.querySelector("#chart svg");
+
+//     chart.setAttribute("height", "650");
+//     chart.setAttribute("width", "650");
+
+//     console.log();
+    
+// });
